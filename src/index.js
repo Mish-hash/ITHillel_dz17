bed2014b28eb0d7a65be9295167de1d26d1b334e
mish-hash <poluektov23@gmail.com>
2022-08-25 15:57:11 +0300

    Add POST request on index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express();
 const port = 8081
 
+app.use(express.json());
+
 app.get('/api/test', (req, res) => {
   res.send(JSON.stringify({text: 'Hello world'}))
 })
@@ -9,6 +11,12 @@ app.get('/api/test', (req, res) => {
 app.post('/api/test', (req, res) => {
   console.log(req)
   res.send('Hello World!')
+})
+
+app.post('/', (req, res) => {
+  if (req.headers['content-type'] === 'application/json') {
+    res.json(req.body);
+  }
 })
 
 app.use(express.static('src'));
